a99b76e616d0bb7ef72b6cd871a0d40917711cc6
Murry Febriansyah putra -niomic <murryfebriansyah1@gmail.com>
2021-01-31 19:31:41 +0700

    cache delete

--- application/controllers/superadmin/karyawan.php
+++ application/controllers/superadmin/karyawan.php
@@ -16,11 +16,9 @@ class Karyawan extends CI_Controller
 
 	public function index()
 	{
-<<<<<<< HEAD
 		$data['url']=  $this->uri->segment(2);
 		$this->load->view('template/header',$data);
 		$this->load->view('superadmin/karyawan');
-=======
 		$karyawan = $this->m_admin->getData("tbl_pegawai")->result();
 		$data = array(
 			'karyawan' =>  $karyawan
@@ -28,35 +26,29 @@ class Karyawan extends CI_Controller
 		//$this->output->cache(1);
 		$this->load->view('template/header');
 		$this->load->view('superadmin/karyawan',$data);
->>>>>>> f5dcb73bca0a59d826e1f565baf1ef8a2058ab2f
 		$this->load->view('template/footer');
 		
 	}
 
 	public function LoadModal()
 	{
-<<<<<<< HEAD
-=======
+
 		//$this->output->cache(1);
->>>>>>> f5dcb73bca0a59d826e1f565baf1ef8a2058ab2f
 		$this->load->view('superadmin/modal_karyawan');
 		
 	}
 
 	public function Edit_karyawan($id)
 	{
-<<<<<<< HEAD
 		$data['url'] = $this->uri->segment(2);
 		$this->load->view('template/header',$data);
 		$this->load->view('superadmin/edit_karyawan');
-=======
 		$data = array(
 			'karyawan'		=> $this->m_admin->cari(array("id_user"=> $id),"tbl_pegawai")->row()
 		);
 		//$this->output->cache(1);
 		$this->load->view('template/header');
 		$this->load->view('superadmin/edit_karyawan',$data);
->>>>>>> f5dcb73bca0a59d826e1f565baf1ef8a2058ab2f
 		$this->load->view('template/footer');
 	}
 }
