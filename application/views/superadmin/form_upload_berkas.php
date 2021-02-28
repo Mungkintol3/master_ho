87767157774f73be54792a8fc4ca316fb3f08fbc
Murry Febriansyah putra -niomic <murry.febrian@gmail.com>
2021-02-28 13:47:56 +0700

    form upload

--- application/views/superadmin/form_upload_berkas.php
+++ application/views/superadmin/form_upload_berkas.php
@@ -7,248 +7,54 @@
                   <p class="card-category"> SIGAP PRIMA ASTREA & SIGAP GARDA PRATAMA</p>
                 </div>
                 <div class="card-body">
-                  
-                    <div class="form-group">
-                      <button type="button " data-toggle="modal" data-target="#selectkaryawan" class="btn btn-success">Cari Karyawan <i class="fa fa-search"></i> </button>
-                    </div>
-                  <form id="formtraining" enctype="multipart/form-data" class="form-horizontal">
-                    <div class="form-group">
-                      <input type="hidden" name="id" id="id">
-                      <input type="hidden" name="id_user" id="id_user">
-                      <input readonly="" type="text" id="nama" name="nama" placeholder="Enter Nama" class="form-control">
-                    </div>
 
-                    <div class="form-group">
-                      <input readonly="" type="text" name="npk" id="npk" placeholder="Enter NPK" class="form-control">
-                    </div>
+ <div class="form-group form-file-upload form-file-multiple">
+    <input type="file" multiple="" class="inputFileHidden">
+    <div class="input-group">
+        <input type="text" class="form-control form-file-upload inputFileVisible" placeholder="Single File">
+        <span class="input-group-btn">
+            <button type="button" class="btn btn-fab btn-round btn-primary">
+                <i class="material-icons">attach_file</i>
+            </button>
+        </span>
+    </div>
+  </div>
 
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Kartu Tanda Penduduk </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                 <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Kartu Keluarga </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                 <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Nomor Pokok Wajib Pajak </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Buku Rekening </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Surat Lamaran </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Daftar Riwayat Hidup </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                    <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Ijazah Pendidikan </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Keterangan Domisili </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Surat Kesehatan </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Surat Keterangan Catatan Kepolisian </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>
-
-                  <div class="form-group form-file-upload form-file-multiple">
-                    <input type="file" multiple="" class="inputFileHidden">
-                    <label> Foto  </label>
-                    <div class="input-group">
-                    <input type="text" class="form-control inputFileVisible" placeholder="">
-                    <span class="input-group-btn">
-                    <button type="button" class="btn btn-fab btn-round btn-primary">
-                    <i class="material-icons">attach_file</i>
-                    </button>
-                    </span>
-                   </div>
-                  </div>   
-                  <button class="btn btn-info">Simpan Perubahan</button>               
- <!-- modal form tambah jabatan -->
-<!--  <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="selectkaryawan" class="modal fade">
-     <div class="modal-dialog modal-xl">
-         <div class="modal-content">
-             <div class="modal-header">
-             Data Karyawan
-                 <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
-             </div>
-             <div class="modal-body" id="#">
-                <table class="table">
-                  <tr>
-                    <th>No</th>
-                    <th>Nama</th>
-                    <th>NPK</th>
-                  </tr>
-                  <tbody>
-                    <?php $no = 1 ; foreach($karyawan as $f) : ?>
-                    <tr>
-                      <td><?= $no++ ?></td>
-                      <td>
-                        <a class="btn btn-success btn-sm click"
-                           data-npk="<?= $f->npk ?>" 
-                           data-id_user="<?= $f->id_user ?>" 
-                           data-id="<?= $f->id ?>"
-                           data-nama="<?= $f->nama ?>"
-                           >
-                            <?= $f->nama ?>
-                        </a>
-                      </td>
-                      <td><?= $f->npk ?></td>
-                    </tr>
-                    <?php endforeach ?>
-                  </tbody>
-                </table>
-                 <small class="text-danger"><i>*klik nama untuk pilih karyawan*</i> </small>
-                </div>
-             </div>
-         </div>
-     </form>
-     </div> -->
-<!-- end of modal  -->
 
 
   <script type="text/javascript">
-    $('.click').on('click',function(e){
-        document.getElementById("npk").value = $(this).attr('data-npk');
-        document.getElementById("nama").value = $(this).attr('data-nama');
-        document.getElementById("id").value = $(this).attr('data-id');
-        document.getElementById("id_user").value = $(this).attr('data-id_user');
-        $('#selectkaryawan').modal('hide');
-    })
+    // FileInput
+  $('.form-file-simple .inputFileVisible').click(function() {
+    $(this).siblings('.inputFileHidden').trigger('click');
+  });
 
-    $(function(){
-      $("#formtraining").on('submit',function(e){
-        var postData = new FormData(this);
-        e.preventDefault();
-          if(document.getElementById('nama').value == ""){
-            alert("data karyawan masih kosong");
-          }else if(document.getElementById('jenis_training').value == ""){
-            alert("jenis training masih kosong");
-          }else if(document.getElementById('keterangan').value == ""){
-            alert("status masih kosong");
-          }else if(document.getElementById('tanggal').value == ""){
-            alert("tanggal masih kosong");
-          }else if(document.getElementById('file').value == ""){
-            alert("berkas pendukung masih kosong");
-          }  else {
-            $.ajax({
-              url : "<?= base_url('superadmin/Training_histori/add') ?>" ,
-              method : "POST" ,
-              data : postData ,
-              processData : false ,
-              contentType : false ,
-              cache : false ,
-              success : function(e){
-                if(e = "sukses"){
-                  alert(e)
-                  window.location.href="<?= base_url('superadmin/Upload_berkas/') ?>"
-                }else {
-                  alert('gagal');
-                }
-              }
-            })
-          }
-      })
-    })
+  $('.form-file-simple .inputFileHidden').change(function() {
+    var filename = $(this).val().replace(/C:\\fakepath\\/i, '');
+    $(this).siblings('.inputFileVisible').val(filename);
+  });
+
+  $('.form-file-multiple .inputFileVisible, .form-file-multiple .input-group-btn').click(function() {
+    $(this).parent().parent().find('.inputFileHidden').trigger('click');
+    $(this).parent().parent().addClass('is-focused');
+  });
+
+  $('.form-file-multiple .inputFileHidden').change(function() {
+    var names = '';
+    for (var i = 0; i < $(this).get(0).files.length; ++i) {
+      if (i < $(this).get(0).files.length - 1) {
+        names += $(this).get(0).files.item(i).name + ',';
+      } else {
+        names += $(this).get(0).files.item(i).name;
+      }
+    }
+    $(this).siblings('.input-group').find('.inputFileVisible').val(names);
+  });
+
+  $('.form-file-multiple .btn').on('focus', function() {
+    $(this).parent().siblings().trigger('focus');
+  });
+
+  $('.form-file-multiple .btn').on('focusout', function() {
+    $(this).parent().siblings().trigger('focusout');
+  });
   </script>
